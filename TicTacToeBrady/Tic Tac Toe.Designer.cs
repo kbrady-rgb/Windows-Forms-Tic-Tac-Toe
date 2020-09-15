f84970db976bf47f221f5df64a3918d59a96eda7
kbrady-rgb <58741813+kbrady-rgb@users.noreply.github.com>
2020-09-15 15:57:42 -0500

    Partially working
    Does not detect when there is a tie

---
+++ TicTacToeBrady/Tic Tac Toe.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace TicTacToeBrady
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTile1 = new System.Windows.Forms.Label();
+            this.lblWinnerWinnerChickenDinner = new System.Windows.Forms.Label();
+            this.lblTile2 = new System.Windows.Forms.Label();
+            this.lblTile7 = new System.Windows.Forms.Label();
+            this.lblTile6 = new System.Windows.Forms.Label();
+            this.lblTile4 = new System.Windows.Forms.Label();
+            this.lblTile3 = new System.Windows.Forms.Label();
+            this.lblTile8 = new System.Windows.Forms.Label();
+            this.lblTile9 = new System.Windows.Forms.Label();
+            this.lblTile5 = new System.Windows.Forms.Label();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTile1
+            // 
+            this.lblTile1.AutoSize = true;
+            this.lblTile1.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTile1.Location = new System.Drawing.Point(17, 11);
+            this.lblTile1.Name = "lblTile1";
+            this.lblTile1.Size = new System.Drawing.Size(115, 113);
+            this.lblTile1.TabIndex = 0;
+            this.lblTile1.Text = "X";
+            // 
+            // lblWinnerWinnerChickenDinner
+            // 
+            this.lblWinnerWinnerChickenDinner.AutoSize = true;
+            this.lblWinnerWinnerChickenDinner.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblWinnerWinnerChickenDinner.Location = new System.Drawing.Point(139, 392);
+            this.lblWinnerWinnerChickenDinner.Name = "lblWinnerWinnerChickenDinner";
+            this.lblWinnerWinnerChickenDinner.Size = new System.Drawing.Size(130, 19);
+            this.lblWinnerWinnerChickenDinner.TabIndex = 9;
+            this.lblWinnerWinnerChickenDinner.Text = "And the winner is...";
+            // 
+            // lblTile2
+            // 
+            this.lblTile2.AutoSize = true;
+            this.lblTile2.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTile2.Location = new System.Drawing.Point(154, 11);
+            this.lblTile2.Name = "lblTile2";
+            this.lblTile2.Size = new System.Drawing.Size(115, 113);
+            this.lblTile2.TabIndex = 10;
+            this.lblTile2.Text = "X";
+            // 
+            // lblTile7
+            // 
+            this.lblTile7.AutoSize = true;
+            this.lblTile7.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTile7.Location = new System.Drawing.Point(17, 267);
+            this.lblTile7.Name = "lblTile7";
+            this.lblTile7.Size = new System.Drawing.Size(115, 113);
+            this.lblTile7.TabIndex = 11;
+            this.lblTile7.Text = "X";
+            // 
+            // lblTile6
+            // 
+            this.lblTile6.AutoSize = true;
+            this.lblTile6.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTile6.Location = new System.Drawing.Point(290, 143);
+            this.lblTile6.Name = "lblTile6";
+            this.lblTile6.Size = new System.Drawing.Size(115, 113);
+            this.lblTile6.TabIndex = 12;
+            this.lblTile6.Text = "X";
+            // 
+            // lblTile4
+            // 
+            this.lblTile4.AutoSize = true;
+            this.lblTile4.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTile4.Location = new System.Drawing.Point(17, 143);
+            this.lblTile4.Name = "lblTile4";
+            this.lblTile4.Size = new System.Drawing.Size(115, 113);
+            this.lblTile4.TabIndex = 13;
+            this.lblTile4.Text = "X";
+            // 
+            // lblTile3
+            // 
+            this.lblTile3.AutoSize = true;
+            this.lblTile3.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTile3.Location = new System.Drawing.Point(290, 11);
+            this.lblTile3.Name = "lblTile3";
+            this.lblTile3.Size = new System.Drawing.Size(115, 113);
+            this.lblTile3.TabIndex = 14;
+            this.lblTile3.Text = "X";
+            // 
+            // lblTile8
+            // 
+            this.lblTile8.AutoSize = true;
+            this.lblTile8.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTile8.Location = new System.Drawing.Point(154, 267);
+            this.lblTile8.Name = "lblTile8";
+            this.lblTile8.Size = new System.Drawing.Size(115, 113);
+            this.lblTile8.TabIndex = 15;
+            this.lblTile8.Text = "X";
+            // 
+            // lblTile9
+            // 
+            this.lblTile9.AutoSize = true;
+            this.lblTile9.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTile9.Location = new System.Drawing.Point(290, 267);
+            this.lblTile9.Name = "lblTile9";
+            this.lblTile9.Size = new System.Drawing.Size(115, 113);
+            this.lblTile9.TabIndex = 16;
+            this.lblTile9.Text = "X";
+            // 
+            // lblTile5
+            // 
+            this.lblTile5.AutoSize = true;
+            this.lblTile5.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTile5.Location = new System.Drawing.Point(154, 143);
+            this.lblTile5.Name = "lblTile5";
+            this.lblTile5.Size = new System.Drawing.Size(115, 113);
+            this.lblTile5.TabIndex = 17;
+            this.lblTile5.Text = "X";
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(36, 439);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(109, 35);
+            this.btnGo.TabIndex = 18;
+            this.btnGo.Text = "&New Game";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.BtnGo_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.Location = new System.Drawing.Point(274, 439);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(109, 35);
+            this.btnExit.TabIndex = 19;
+            this.btnExit.Text = "&Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.BtnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnGo;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(436, 509);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.lblTile5);
+            this.Controls.Add(this.lblTile9);
+            this.Controls.Add(this.lblTile8);
+            this.Controls.Add(this.lblTile3);
+            this.Controls.Add(this.lblTile4);
+            this.Controls.Add(this.lblTile6);
+            this.Controls.Add(this.lblTile7);
+            this.Controls.Add(this.lblTile2);
+            this.Controls.Add(this.lblWinnerWinnerChickenDinner);
+            this.Controls.Add(this.lblTile1);
+            this.Name = "Form1";
+            this.Text = "Tic Tac Toe";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTile1;
+        private System.Windows.Forms.Label lblWinnerWinnerChickenDinner;
+        private System.Windows.Forms.Label lblTile2;
+        private System.Windows.Forms.Label lblTile7;
+        private System.Windows.Forms.Label lblTile6;
+        private System.Windows.Forms.Label lblTile4;
+        private System.Windows.Forms.Label lblTile3;
+        private System.Windows.Forms.Label lblTile8;
+        private System.Windows.Forms.Label lblTile9;
+        private System.Windows.Forms.Label lblTile5;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ TicTacToeBrady/Tic Tac Toe.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/***************************************************************
+* Name        : Tic Tac Toe
+* Author      : Kabrina Brady
+* Created     : 10/8/2019
+* Course      : CIS 169 - C#
+* Version     : 1.0
+* OS          : Windows 10
+* Copyright   : This is my own original work
+* Description : Player clicks New Game button and computer randomly fills tic tac toe board and displays who won.
+*               Input:  Button clicks
+*               Output: Filled array
+* Academic Honesty: I attest that this is my original work.
+* I have not used unauthorized source code, either modified or unmodified. I have not given other fellow student(s) access to my program.         
+***************************************************************/
+
+namespace TicTacToeBrady
+{
+    public partial class Form1 : Form
+    {
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnExit_Click(object sender, EventArgs e)
+        {
+            this.Close(); //closes form
+        }
+
+        private void FillArray(int[] iArray)
+        {
+
+        }
+
+        private void BtnGo_Click(object sender, EventArgs e)
+        {
+            //Makes all tiles display X
+            lblTile1.Text = "X";
+            lblTile2.Text = "X";
+            lblTile3.Text = "X";
+            lblTile4.Text = "X";
+            lblTile5.Text = "X";
+            lblTile6.Text = "X";
+            lblTile7.Text = "X";
+            lblTile8.Text = "X";
+            lblTile9.Text = "X";
+
+            Random rand = new Random(); //creates new random object
+
+            //Every tile needs a separate variable so that all tiles won't be assigned the same letter
+            string randomValueString; //string to hold an X or O
+            string randomValueString2; //string to hold an X or O
+            string randomValueString3; //string to hold an X or O
+            string randomValueString4; //string to hold an X or O
+            string randomValueString5; //string to hold an X or O
+            string randomValueString6; //string to hold an X or O
+            string randomValueString7; //string to hold an X or O
+            string randomValueString8; //string to hold an X or O
+            string randomValueString9; //string to hold an X or O
+
+            int ROWS = 3;
+            int COLS = 3;
+            int[,] tilesArray = new int[ROWS, COLS]; //parallel array
+            for (int row = 0; row < ROWS; row++)
+            {
+                for (int col = 0; col < COLS; col++)
+                {
+                    tilesArray[row, col] = rand.Next(2); //parallel array now filled with random values 
+                }
+            }
+            //Fills the GUI side of the program with Xs and Os
+            if (tilesArray[0, 0] == 0)
+            {
+                randomValueString = "O";
+            }
+            else
+            {
+                randomValueString = "X";
+            }
+
+            if (tilesArray[0, 1] == 0)
+            {
+                randomValueString2 = "O";
+            }
+            else
+            {
+                randomValueString2 = "X";
+            }
+
+            if (tilesArray[0, 2] == 0)
+            {
+                randomValueString3 = "O";
+            }
+            else
+            {
+                randomValueString3 = "X";
+            }
+
+            if (tilesArray[1, 0] == 0)
+            {
+                randomValueString4 = "O";
+            }
+            else
+            {
+                randomValueString4 = "X";
+            }
+
+            if (tilesArray[1, 1] == 0)
+            {
+                randomValueString5 = "O";
+            }
+            else
+            {
+                randomValueString5 = "X";
+            }
+
+            if (tilesArray[1, 2] == 0)
+            {
+                randomValueString6 = "O";
+            }
+            else
+            {
+                randomValueString6 = "X";
+            }
+
+            if (tilesArray[2, 0] == 0)
+            {
+                randomValueString7 = "O";
+            }
+            else
+            {
+                randomValueString7 = "X";
+            }
+
+            if (tilesArray[2, 1] == 0)
+            {
+                randomValueString8 = "O";
+            }
+            else
+            {
+                randomValueString8 = "X";
+            }
+
+            if (tilesArray[2, 2] == 0)
+            {
+                randomValueString9 = "O";
+            }
+            else
+            {
+                randomValueString9 = "X";
+            }
+            //assigns the values of the variables to the labels' text properties
+            lblTile1.Text = randomValueString;
+            lblTile2.Text = randomValueString2;
+            lblTile3.Text = randomValueString3;
+            lblTile4.Text = randomValueString4;
+            lblTile5.Text = randomValueString5;
+            lblTile6.Text = randomValueString6;
+            lblTile7.Text = randomValueString7;
+            lblTile8.Text = randomValueString8;
+            lblTile9.Text = randomValueString9;
+
+            //Checks arrangement to see who won
+
+            if (lblTile1.Text == "O" && lblTile2.Text == "O" && lblTile3.Text == "O"
+                || lblTile4.Text == "O" && lblTile5.Text == "O" && lblTile6.Text == "O"
+                || lblTile7.Text == "O" && lblTile8.Text == "O" && lblTile9.Text == "O"
+                || lblTile1.Text == "O" && lblTile4.Text == "O" && lblTile7.Text == "O"
+                || lblTile2.Text == "O" && lblTile5.Text == "O" && lblTile8.Text == "O"
+                || lblTile3.Text == "O" && lblTile6.Text == "O" && lblTile9.Text == "O"
+                || lblTile1.Text == "O" && lblTile5.Text == "O" && lblTile9.Text == "O"
+                || lblTile3.Text == "O" && lblTile5.Text == "O" && lblTile7.Text == "O")
+            {
+                lblWinnerWinnerChickenDinner.Text = "Player O won!";
+
+                if (lblTile1.Text == "X" && lblTile2.Text == "X" && lblTile3.Text == "X"
+                || lblTile4.Text == "X" && lblTile5.Text == "X" && lblTile6.Text == "X"
+                || lblTile7.Text == "X" && lblTile8.Text == "X" && lblTile9.Text == "X"
+                || lblTile1.Text == "X" && lblTile4.Text == "X" && lblTile7.Text == "X"
+                || lblTile2.Text == "X" && lblTile5.Text == "X" && lblTile8.Text == "X"
+                || lblTile3.Text == "X" && lblTile6.Text == "X" && lblTile9.Text == "X"
+                || lblTile1.Text == "X" && lblTile5.Text == "X" && lblTile9.Text == "X"
+                || lblTile3.Text == "X" && lblTile5.Text == "X" && lblTile7.Text == "X")
+                {
+                    lblWinnerWinnerChickenDinner.Text = "Player X won!";
+                }
+            }
+            else if ((lblTile1.Text == "X" && lblTile2.Text == "X" && lblTile3.Text == "X" && lblTile4.Text == "O" && lblTile5.Text == "O" && lblTile6.Text == "O") 
+                || (lblTile1.Text == "O" && lblTile2.Text == "O" && lblTile3.Text == "O" && lblTile4.Text == "X" && lblTile5.Text == "X" && lblTile6.Text == "X")
+                || (lblTile4.Text == "O" && lblTile5.Text == "O" && lblTile6.Text == "O" && lblTile7.Text == "X" && lblTile8.Text == "X" && lblTile9.Text == "X")
+                || (lblTile4.Text == "X" && lblTile5.Text == "X" && lblTile6.Text == "X" && lblTile7.Text == "O" && lblTile8.Text == "O" && lblTile9.Text == "O")
+                || (lblTile1.Text == "X" && lblTile4.Text == "X" && lblTile7.Text == "X" && lblTile2.Text == "O" && lblTile5.Text == "O" && lblTile8.Text == "O")
+                || (lblTile1.Text == "O" && lblTile4.Text == "O" && lblTile7.Text == "O" && lblTile2.Text == "X" && lblTile5.Text == "X" && lblTile8.Text == "X")
+                || (lblTile2.Text == "X" && lblTile5.Text == "X" && lblTile8.Text == "X" && lblTile3.Text == "O" && lblTile6.Text == "O" && lblTile9.Text == "O")
+                || (lblTile2.Text == "O" && lblTile5.Text == "O" && lblTile8.Text == "O" && lblTile3.Text == "X" && lblTile6.Text == "X" && lblTile9.Text == "X")
+                )
+            {
+                lblWinnerWinnerChickenDinner.Text = "It's a tie!";
+            }
+            else
+            {
+                lblWinnerWinnerChickenDinner.Text = "No one won. Try again.";
+            }
+        }
+    }
+}
